760bf53a939581f9186be8f5cf64864e06fde4fc
Jae Yong Kim <kim900180@gmail.com>
2018-07-29 19:56:25 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,15 +1,4 @@
 # Add  code here!
 def prime?(number)
-  dividers = (2..5).to_a
-  dividers.each do |divider|
-    if number == 2
-      return true
-    elsif number % divider == 0
-      return false
-    elsif number < 0 || number == 1
-      return false
-    else
-      return true
-    end
-  end
+   number.prime?  
 end
